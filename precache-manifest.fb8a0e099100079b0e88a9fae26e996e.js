c0d6b7d22e8caada92165ea4c235c8b4db12865a
pragyak412 <pragyak412@gmail.com>
2020-04-27 23:58:13 +0530

    Updates

---
+++ precache-manifest.fb8a0e099100079b0e88a9fae26e996e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908fb0c043e4fc2f872ffc2dd72ea7ee",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "0e0b61634413d7194da3",
+    "url": "/my_app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1144fb737b1fb6c67742",
+    "url": "/my_app/static/js/2.63e0e1a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_app/static/js/2.63e0e1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0b61634413d7194da3",
+    "url": "/my_app/static/js/main.d71ed3de.chunk.js"
+  },
+  {
+    "revision": "efbdde198cf75b71f852",
+    "url": "/my_app/static/js/runtime-main.2f5fcbe1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
